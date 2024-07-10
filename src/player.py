5c9805327f25bf2528b7e0b38dcb954a8264c844
HmarVR <i.am.a.dum.idiot@outlook.com>
2024-07-10 12:27:39 +0300

    Movmen

--- src/player.py
+++ src/player.py
@@ -1,77 +1,203 @@
 import pygame as pg
+import glm
+import struct
 import math
-import time 
-
-from player.rigidBody import RigidBody
 from bindings import *
 
-class Player(RigidBody):
+neighbor_offsets = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1), (0, 0)]
+
+class RigidBody:
     def __init__(self):
+        self.rect = pg.FRect(0, 0, 16, 16)
+        self.velocity = [0, 0]
+        self.collision_types = {'bottom': False, 'top': False, 'right': False, 'left': False}
+        self.coyote_time = 0
+        self.coyote_time_wall = 0
+        self.elasticity = 0
+        self.friction = [1.0, 0.5]
+
+    def get_neighboring_tiles(self, tilemap):
+        tiles = []
+        loc = (round(self.rect.x // 16), round(self.rect.y // 16))
+
+        for offset in neighbor_offsets:
+            around_loc = f'{offset[0] + loc[0]};{offset[1] - loc[1]}'
+            if around_loc in tilemap:
+                if tilemap[around_loc][1]:
+                    x, y = around_loc.split(';')
+                    x = (int(x)) * 16
+                    y = (-int(y)) * 16
+                    r = pg.Rect(x, y, 16, 16)
+                    tiles.append(r)
+        
+        return tiles
+
+    def collision_test(self, rect, tileset):
+        hit_list = []
+        
+        for tile in self.get_neighboring_tiles(tileset):
+            if rect.colliderect(tile):
+                hit_list.append(tile)
+
+        return hit_list
+
+    def apply_physics(self, tilemap, dt):
+        self.collision_types = {'bottom': False, 'top': False, 'right': False, 'left': False}
+
+        self.rect.x += self.velocity[0] * dt
+
+        hit_list = self.collision_test(self.rect, tilemap)
+
+        for block in hit_list:
+            if self.velocity[0] > 0:
+                self.rect.right = block.left
+                self.collision_types['left'] = True
+                self.coyote_time_wall = 0
+
+            if self.velocity[0] < 0:
+                self.rect.left = block.right
+                self.collision_types['right'] = True
+                self.coyote_time_wall = 0
+
+            if self.friction[1] == 0 or self.friction[1] == 1:
+                self.velocity[1] -= self.velocity[1] * self.friction[1]
+            else:
+                self.velocity[1] += self.velocity[1] * self.friction[1] * dt
+                
+            self.velocity[0] = -self.velocity[0] * self.elasticity
+        
+            break
+
+        self.rect.y += self.velocity[1] * dt
+        hit_list = self.collision_test(self.rect, tilemap)
+        
+        do_gravity = 392
+        self.velocity[1] -= do_gravity * dt
+        self.velocity[1] = max(min(self.velocity[1], 300), -300)
+        
+        for block in hit_list:
+            if self.velocity[1] < 0:
+                self.rect.top = block.bottom
+                self.collision_types['bottom'] = True
+                self.coyote_time = 0
+
+            if self.velocity[1] > 0:
+                self.rect.bottom = block.top
+                self.collision_types['top'] = True
+
+
+            if self.friction[0] == 0 or self.friction[0] == 1:
+                self.velocity[0] -= self.velocity[0] * self.friction[0]
+            else:
+                self.velocity[0] -= self.velocity[0] * self.friction[0] * dt
+            
+            
+            self.velocity[1] = -self.velocity[1] * self.elasticity
+            break
+            
+        
+        
+        
+
+class Player(RigidBody):
+    def __init__(self, app):
         super().__init__()
-        self.elasticity = 0.3
-        self.friction = 50
-        self.reach = 30
-        self.start_mining = None
-        self.block_mining = None
-        self.mining_amp = 1
+        self.app = app
+        
+        self.app.mesh.vao.add_vao(
+            vao_name="player",
+            fbo=self.app.mesh.vao.Framebuffers.framebuffers["default"],
+            program=self.app.mesh.vao.program.programs['player'],
+            vbo=self.app.mesh.vao.vbo.vbos['plane'],
+            umap=
+			{ 
+				"m_model": "mat4",
+				"m_view": "mat4",
+				"frame": "int",
+			},
+            tmap=["u_texture_0"],
+        )
+        self.vao = self.app.mesh.vao.vaos['player']
+        
+        self.texture = self.app.mesh.texture.textures["player"]
+        self.vao.texture_bind(0, "u_texture_0", self.texture)
+        
+        self.pos = glm.vec3(900, 0, 0)
+        self.rect = pg.FRect(900, 0, 15, 16)
+        self.roll = 0
+        self.scale = glm.vec2(8)
+        self.boxcam = glm.vec2(0)
+        self.frame = 0
+        self.time_since_lastframe = 0
 
     def check(self, keys):
-        self.velocity[0] = 0
+        # self.velocity[0] = 0
+        MAX_SPED = 100
+        ACC_SPED = 200
+        MAX_ACCEL = glm.clamp(ACC_SPED-abs(self.velocity[0]), 0, MAX_SPED)
+        
+        MAX_WINDSPED = 50
+        ACC_WINDSPED = 100
+        MAX_WINDACCEL = glm.clamp(ACC_WINDSPED-abs(self.velocity[0]), 0, MAX_WINDSPED)
 
         if keys[bindings['left']] and keys[bindings['right']]:
-            self.velocity[0] = 0
+            pass
 
         elif keys[bindings['right']]:
-            self.velocity[0] = 120
+            self.velocity[0] += MAX_ACCEL if self.coyote_time < 0.1 else MAX_WINDACCEL
 
         elif keys[bindings['left']]:
-            self.velocity[0] = -120
+            self.velocity[0] -= MAX_ACCEL if self.coyote_time < 0.1 else MAX_WINDACCEL
 
         if self.coyote_time < 0.1 and keys[bindings['jump']]:
-            self.velocity[1] = -350
+            self.velocity[1] = 200
             self.coyote_time = 100
-
-    def mine(self, direction, tilemap, pressed):
-        if pressed:
-            collided_tiles = []
-
-            new_pos = [*self.rect.center]
-            new_pos[0] += math.cos(direction) * self.reach
-            new_pos[1] += math.sin(direction) * self.reach
-
-            for tile in tilemap:
-                pos = tile.split(';')
-                pos[0] = 16 * int(pos[0])
-                pos[1] = 16 * int(pos[1])
-                
-                r = pg.Rect(pos[0], pos[1], 16, 16)
-                if r.clipline(self.rect.center, new_pos):
-                    collided_tiles.append((tile, r))
-
-            closest_tile = [None, float('inf')]        
-
-            for tile, rect in collided_tiles:
-                dist = math.hypot(rect.y - self.rect.y, rect.x - self.rect.x)
-
-                if dist < closest_tile[1]:
-                    closest_tile = [tile, dist]
-
-            del collided_tiles
-
-            if tilemap[tile][1]:
-                if self.block_mining != tile:
-                    self.start_mining = time.time()
-                    self.block_mining = tile
-
-                if not self.start_mining:
-                    self.start_mining = time.time()
-                    self.block_mining = tile
-                    
-                if (time.time() - self.start_mining) * self.mining_amp > tilemap[tile][5]:
-                    self.start_mining = 0
-                    return closest_tile[0]
+            
+        elif self.coyote_time_wall < 0.1 and keys[bindings['jump']]:
+            self.velocity[1] = 100
+            self.velocity[0] = -480 if self.collision_types['left'] else 480
+            self.coyote_time = 100
+            
+            
+            
+    def update(self):
+        self.apply_physics(self.app.share_data['tilemap'].tilemap['0'], self.app.delta_time)
+        self.check(pg.key.get_pressed())
+        self.coyote_time += self.app.delta_time
+        self.coyote_time_wall += self.app.delta_time
         
-        else:
-            self.start_mining = 0
+        self.boxcam = glm.clamp(self.boxcam, glm.vec2(-30), glm.vec2(30))
+        self.boxcam += self.pos.xy - glm.vec2(self.rect.x-1, self.rect.y)
+        
+        self.pos = glm.vec3(self.rect[0]-1, self.rect[1], 0)
+        
+        
+        
+        self.app.camera.position.xy = self.pos.xy + self.boxcam
+        self.m_model = self.get_model_matrix()
+        
+        self.time_since_lastframe += self.app.delta_time
+        if self.time_since_lastframe > 0.288:
+            self.frame += 1
+            self.frame = self.frame%4
+            self.time_since_lastframe = 0
+        
+        self.vao.uniform_bind("m_model", self.m_model.to_bytes())
+        self.vao.uniform_bind("frame", struct.pack("i", self.frame))
+        self.vao.uniform_bind(
+            "m_view",
+            (self.app.camera.m_proj * self.app.camera.m_view * self.m_model).to_bytes(),
+        )
+        
+        self.vao.render()
 
-        return None
+    def get_model_matrix(self):
+        m_model = glm.mat4()
+        # translate
+        m_model = glm.translate(m_model, self.pos)
+        # rotate
+        m_model = glm.rotate(m_model, glm.radians(self.roll), glm.vec3(0, 0, 1))
+        # scale
+        m_model = glm.scale(m_model, glm.vec3(self.scale[0], self.scale[1], 1))
+        return m_model
+    
